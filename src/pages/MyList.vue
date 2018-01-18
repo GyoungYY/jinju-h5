65c6c41207a0a9d4207edf76a857aba6142ac88b
yangguang <yangguang@bilibili.com>
2018-01-18 13:49:47 +0800

    我的页面

--- src/pages/MyList.vue
+++ src/pages/MyList.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <div class="photo-div">
+            <img src="../../static/img/photo0.jpeg" alt="" class="my-photo">
+        </div>
         <mt-cell title="个人信息"></mt-cell>
         <mt-cell title="发表金句"></mt-cell>
         <mt-cell title="退出登录"></mt-cell>
@@ -8,18 +11,25 @@
 
 <script>
     export default {
-        data(){
-            return{
-
-            }
+        data() {
+            return {}
         },
 
-        methods:{
-
-        }
+        methods: {}
     }
 </script>
 
-<style>
+<style scoped>
+    .photo-div {
+        text-align: center;
+        padding: 20px;
+        background-color: #fff;
+    }
 
+    .my-photo {
+        width: 90px;
+        height: 90px;
+        border-radius: 45px;
+        border: 1px solid #ddd;
+    }
 </style>
