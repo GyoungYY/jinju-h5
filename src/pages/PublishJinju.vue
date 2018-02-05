3a54e5031323b88a7dfda2a6d1b102b5fe9a17f3
yangguang <yangguang@bilibili.com>
2018-02-05 15:10:09 +0800

    发表金句去掉scoped

--- src/pages/PublishJinju.vue
+++ src/pages/PublishJinju.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="publish-container">
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="top"label-width="100px" class="demo-ruleForm">
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="top" label-width="100px"
+                 class="demo-ruleForm">
             <el-form-item label="金句类型" prop="type">
                 <el-select v-model="ruleForm.type" placeholder="请选择金句类型">
                     <el-option label="搞笑" :value="1"></el-option>
@@ -9,7 +10,8 @@
                 </el-select>
             </el-form-item>
             <el-form-item label="金句内容" prop="content">
-                <el-input type="textarea" v-model="ruleForm.content" :autosize="{ minRows: 4, maxRows: 6}" placeholder="请输入内容"
+                <el-input type="textarea" v-model="ruleForm.content" :autosize="{ minRows: 4, maxRows: 6}"
+                          placeholder="请输入内容"
                           :maxlength="500"></el-input>
             </el-form-item>
             <el-form-item style="margin: 0;">{{ruleForm.content.length}}/500</el-form-item>
@@ -42,7 +44,7 @@
             }
         },
 
-        methods:{
+        methods: {
             //立即创建
             submitForm(formName) {
                 if (!this.userInfo) {
@@ -73,9 +75,14 @@
     }
 </script>
 
-<style scoped>
-    .publish-container{
+<style>
+    .publish-container {
         padding: 20px 10px;
         background-color: #fff;
+    }
+
+    .publish-container .el-form--label-top .el-form-item__label {
+        padding: 0 !important;
+        margin: 0 !important;
     }
 </style>
