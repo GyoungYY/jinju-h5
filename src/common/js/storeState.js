873211a46a6c84451a0485589d638f38403a9e75
yangguang <yangguang@bilibili.com>
2018-02-08 15:24:20 +0800

    【性能优化】引入vue和element-ui cdn文件

--- src/common/js/storeState.js
+++ src/common/js/storeState.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 import vuex from 'vuex'
 
 Vue.use(vuex);
--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+
+/*********性能优化**************
+ * 【1】进行vue-router路由组件懒加载
+ *
+ * 【2】引入jquery和bootstrap cdn文件：注释main.js中import，再index.html文件中引入
+ * 【3】引入element-ui/lib/theme-chalk/index.css cdn文件：同上
+ * 【4】引入vue和element-ui cdn文件：注释main.js中import element-ui、其他所有地方import vue
+ ******************************/
+
+// import Vue from 'vue'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
